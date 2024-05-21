3629f12c2232a0d660b3248e32d151eb5b9bc5c1
Shawn Peery <shawn.w8465@gmail.com>
2024-05-21 15:56:44 -0600

    Going to try different approach

--- src/main/java/com/shawn/firstforgemod/listener/ControllableListener.java
+++ src/main/java/com/shawn/firstforgemod/listener/ControllableListener.java
@@ -12,17 +12,48 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = FirstForgeMod.MODID)
 public class ControllableListener implements ControllerEvents.Button {
 
+    private static boolean interatingLocked = false;
+
+    private static Float previousRValue = null;
+
     @Override
     public boolean handle(Controller controller) {
 
-        System.out.println("Right Trigger Value:");
-        System.out.println(controller.getRTriggerValue());
 
-        System.out.println("Name: controller.getName()");
-        System.out.println(controller.getName());
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
+        float rTriggerValue = controller.getRTriggerValue();
+
+
+        System.out.printf("rTriggerValue: %f%n", rTriggerValue);
+
+        System.out.printf("previousRValue: %f%n", previousRValue);
+        if (previousRValue == null) {
+
+            System.out.print("previousRValue is null. setting it to previous r value and returning.");
+            previousRValue = rTriggerValue;
+            return false;
+        }
+
+        System.out.printf("previousRValue is not null!");
+
+        if (previousRValue >= rTriggerValue) {
+
+            System.out.print("previousRValue is greaterthan or equal to rTriggerValue. Returning");
+            return false;
+        }
+
+        interatingLocked = !interatingLocked;
+
+        System.out.print("Toggling Mining. New value:" + interatingLocked);
 
         KeyMapping.click(Minecraft.getInstance().options.keyAttack.getKey());
-        Minecraft.getInstance().options.keyAttack.setDown(true);
+        Minecraft.getInstance().options.keyAttack.setDown(interatingLocked);
         return false;
+
     }
 }
