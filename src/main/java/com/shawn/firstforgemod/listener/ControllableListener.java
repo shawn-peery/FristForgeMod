f5ecc3d369bc7943e0e73dc9786c2fd42bd2644e
Shawn Peery <shawn.w8465@gmail.com>
2024-05-21 15:37:43 -0600

    Getting close

--- src/main/java/com/shawn/firstforgemod/listener/ControllableListener.java
+++ src/main/java/com/shawn/firstforgemod/listener/ControllableListener.java
@@ -3,50 +3,26 @@ package com.shawn.firstforgemod.listener;
 import com.mrcrayfish.controllable.client.input.Controller;
 import com.mrcrayfish.controllable.event.ControllerEvents;
 import com.shawn.firstforgemod.FirstForgeMod;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-//import com.mrcrayfish.controllable.event.RenderPlayerPreviewEvent;
-
-
-import com.mrcrayfish.controllable.event.ControllerEvents;
-import com.shawn.firstforgemod.FirstForgeMod;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.Mod;
 
 
-import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = FirstForgeMod.MODID)
 public class ControllableListener implements ControllerEvents.Button {
 
-    //
-    //@SubscribeEvent
-    //public static void handleGettingHurt(LivingHurtEvent event) {
-    //    System.out.println("Something got hurt.");
-    //    if (event.getEntity() != null && event.getEntity() instanceof Player) {
-    //        Player player = (Player) event.getEntity();
-    //        player.sendSystemMessage(Component.literal("You got hurt!"));
-    //    }
-   // }
-
-
-
-   // @SubscribeEvent
-  //  public static void handleInput(ControllerEvents.Button event) {
- //       System.out.println("Item picked up!");
-//
-    //}
-
     @Override
     public boolean handle(Controller controller) {
 
-        System.out.println("Button PRessed!!");
+        System.out.println("Right Trigger Value:");
+        System.out.println(controller.getRTriggerValue());
+
+        System.out.println("Name: controller.getName()");
+        System.out.println(controller.getName());
+
+        KeyMapping.click(Minecraft.getInstance().options.keyAttack.getKey());
+        Minecraft.getInstance().options.keyAttack.setDown(true);
         return false;
     }
 }
